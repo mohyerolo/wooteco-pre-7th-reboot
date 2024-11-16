cc7f896c60f036001dbbfa0425ae32e2cccbb2f6
mohyerolo <garam2101@naver.com>
2024-11-16 14:43:27 +0900

    docs: 첫 번째 미션 ReadMe

--- src/main/java/org/wooteco/pre/calculator/Application.java
+++ src/main/java/org/wooteco/pre/calculator/Application.java
@@ -1,6 +1,6 @@
-package org.wooteco.pre;
+package org.wooteco.pre.calculator;
 
-public class Main {
+public class Application {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
