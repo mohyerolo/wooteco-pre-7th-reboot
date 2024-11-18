0a420201e937782ed73d67d3f46882776ce991c3
mohyerolo <garam2101@naver.com>
2024-11-18 11:44:55 +0900

    feat: 자동차 이름 분리 및 빈 값 검증

---
+++ src/main/java/org/wooteco/pre/racingCar/view/InputView.java
@@ -0,0 +1,19 @@
+package org.wooteco.pre.racingCar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private static final String BLANK_ERROR = "입력값은 비어있을 수 없습니다.";
+
+    public static String read() {
+        String input = Console.readLine();
+        validateBlank(input);
+        return input;
+    }
+
+    private static void validateBlank(String input) {
+        if (input == null || input.isBlank()) {
+            throw new IllegalArgumentException(BLANK_ERROR);
+        }
+    }
+}
