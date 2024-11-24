d78fba2cd99e398b06fdc3d056d6d3ccfd83706a
mohyerolo <garam2101@naver.com>
2024-11-24 22:58:29 +0900

    feat: 프로모션, 상품 만드는 서비스 추가

---
+++ src/main/java/org/wooteco/pre/convenienceStore/service/StoreService.java
@@ -0,0 +1,31 @@
+package org.wooteco.pre.convenienceStore.service;
+
+import org.wooteco.pre.convenienceStore.dao.ProductDao;
+import org.wooteco.pre.convenienceStore.domain.product.ProductFactory;
+import org.wooteco.pre.convenienceStore.domain.promotion.Promotion;
+import org.wooteco.pre.convenienceStore.domain.promotion.PromotionFactory;
+import org.wooteco.pre.convenienceStore.util.FileReaderUtil;
+
+import java.util.List;
+
+public class StoreService {
+    private static final String promotionPath = "src/main/resources/promotions.md";
+    private static final String productPath = "src/main/resources/products.md";
+
+    public void makeStore() {
+        List<Promotion> promotions = createPromotion();
+        createProductStorage(promotions);
+    }
+
+    private List<Promotion> createPromotion() {
+        List<String> promotionData = FileReaderUtil.readFile(promotionPath);
+        promotionData.removeFirst();
+        return PromotionFactory.createPromotions(promotionData);
+    }
+
+    private void createProductStorage(final List<Promotion> promotions) {
+        List<String> productData = FileReaderUtil.readFile(productPath);
+        productData.removeFirst();
+        ProductFactory.createProductStorage(productData, promotions.stream());
+    }
+}
