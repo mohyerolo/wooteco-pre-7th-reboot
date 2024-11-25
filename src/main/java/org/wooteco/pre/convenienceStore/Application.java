593e0b5fc756381d48094983088bf376d6421d24
mohyerolo <garam2101@naver.com>
2024-11-25 17:45:37 +0900

    feat: 실행 기능 추가

--- src/main/java/org/wooteco/pre/convenienceStore/Application.java
+++ src/main/java/org/wooteco/pre/convenienceStore/Application.java
@@ -1,7 +1,12 @@
 package org.wooteco.pre.convenienceStore;
 
+import org.wooteco.pre.convenienceStore.controller.StoreController;
+import org.wooteco.pre.convenienceStore.service.StoreService;
+import org.wooteco.pre.convenienceStore.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-
+        StoreController storeController = new StoreController(new StoreService(), new OutputView());
+        storeController.open();
     }
 }
